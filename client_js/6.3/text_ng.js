304533e22c7126718287c65c8ac26823005aef72
Kensuke Takahara <zawcjk@gmail.com>
2019-11-25 23:55:05 +0900

    finished 6.3

---
+++ client_js/6.3/text_ng.js
@@ -0,0 +1,15 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    document.getElementById("btn").addEventListener(
+      "click",
+      function() {
+        let name = document.getElementById("name");
+        let result = document.getElementById("result");
+        result.innerHTML = "こんにちは、" + name.value + "さん!";
+      },
+      false
+    );
+  },
+  false
+);
